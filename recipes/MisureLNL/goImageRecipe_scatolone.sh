1b8637cb9cd4b9d878e91fc8e70744f96f494b25
Sara Vanini <sara.vanini@pd.infn.it>
2018-03-03 11:00:11 +0000

    Muon rototranslation implemented for LNL run 2756
    git-svn-id: http://mutomweb.pd.infn.it:5214/IB/trunk@1905 1fe49acc-410b-461c-8423-7eec787c53b5

--- recipes/MisureLNL/goImageRecipe_scatolone.sh
+++ recipes/MisureLNL/goImageRecipe_scatolone.sh
@@ -11,11 +11,15 @@ START=0
 AN="IBAnalyzerEM"
 #AN="IBAnalyzerPoca"
 RUN=2756
-FILE=//mnt/mutom-gluster/data/ispra/lnl/pattrec/Radmufit_r${RUN}_0_100000000ev_PR_1Mev.root
+FILE=/mnt/mutom-gluster/data/ispra/lnl/pattrec/Radmufit_r${RUN}_0_100000000ev_PR_1Mev.root
 MIN=280
 VOX=1.5
 #
 ./build/bin/misureLNL ${FILE}  /mnt/mutom-gluster/data/ispra/lnl/vtk/r${RUN}_rototranslat_img_vox${VOX}_${MIN}min  ${MIN} ${START} ${VOX} --detector.transform=true --detector.findMatrix=true --detector.file1="LNLfiles/run2756_ave5.txt" --detector.file2="LNLfiles/run2755_ave5.txt" --iterations='1 1000 5000'
+#
+MIN=2520
+./build/bin/misureLNL ${FILE}  /mnt/mutom-gluster/data/ispra/lnl/vtk/r${RUN}_rototranslat_img_vox${VOX}_${MIN}min  ${MIN} ${START} ${VOX} --detector.transform=true --detector.findMatrix=true --detector.file1="LNLfiles/run2756_ave5.txt" --detector.file2="LNLfiles/run2755_ave5.txt" --iterations='1 1000 5000'
+#
 ### test transformation goodness
 #./build/bin/misureLNL ${FILE}  test_r${RUN}_img_vox${VOX}_${MIN}min  ${MIN} ${START} ${VOX}  --analyzers=${AN} --detector.dump="true" --detector.transform=true --detector.findMatrix=true --detector.voxelReferenceSystem=true --detector.file1="LNLfiles/r2756_block_vox_vertex.txt" --detector.file2="LNLfiles/r2755_block_vox_vertex.txt" --iterations='1 50 50'
 #./build/bin/misureLNL ${FILE}  test_r${RUN}_img_vox${VOX}_${MIN}min  ${MIN} ${START} ${VOX}  --analyzers=${AN} --detector.dump="true" --detector.transform=true --detector.findMatrix=true --detector.voxelReferenceSystem=false --detector.file1="LNLfiles/run2756_ave5.txt" --detector.file2="LNLfiles/run2755_ave5.txt" --iterations='1 50 50'
